499d0bbd039e1227cc2b668993024c97d242fed0
Xavier <tim88776@gmail.com>
2024-08-24 21:13:04 +0800

    get CLI argument of website url

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Get CLI arguments
+	args := os.Args[1:] // os.Args[0] is the program name, skip it
+
+	// Check the number of CLI arguments
+	switch len(args) {
+	case 0:
+		fmt.Println("no website provided")
+		os.Exit(1)
+	case 1:
+		fmt.Printf("starting crawl of: %s", args[0])
+	default:
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
 }
